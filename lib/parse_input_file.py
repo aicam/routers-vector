bbfbc0ccc937f3dff2142d9972aebbac302ce9a7
Daniel Macias <dmacias13@csub.edu>
2023-11-30 17:41:00 -0800

    dmacias

--- lib/parse_input_file.py
+++ lib/parse_input_file.py
@@ -6,13 +6,15 @@ def read_file(file):
     :param file: path to the routing table
     :return: routing table class
     '''
-    f = open(file, 'r')
+    f = open(file, 'r', encoding='utf-8')
     lines = [l.replace("\n", "") for l in f.readlines()]
     routing_table.num_servers = int(lines[0])
     routing_table.num_neighbors = int(lines[1])
     for i in range(2, routing_table.num_neighbors + 3):
         routing_table.servers_ip.append(struct.ServerIPs(lines[i]))
     for i in range(routing_table.num_neighbors + 3, (2 * routing_table.num_neighbors) + 3):
+        if lines == 'infinity':
+            lines = float('inf')
         routing_table.distances.append(struct.Distances(lines[i]))
     self_host = [s for s in routing_table.servers_ip if s.id == 1][0]
     routing_table.self_ip = self_host.ip
